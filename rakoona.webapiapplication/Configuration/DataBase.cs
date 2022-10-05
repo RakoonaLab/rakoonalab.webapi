6228c3f2dadf78933b1e353829323e9b61da0ba8
Enrique Chan <enrique.nec@gmail.com>
2022-10-05 00:25:58 -0500

    Create DataBase.cs

---
+++ rakoona.webapiapplication/Configuration/DataBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using rakoona.webapiapplication.Context;
+using rakoona.webapiapplication.Entities.Models.Seguridad;
+
+namespace rakoona.webapi.Configuration
+{
+    public class Database
+    {
+        public static void  ConfigConnectionString(ref WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        }
+
+    }
+}
