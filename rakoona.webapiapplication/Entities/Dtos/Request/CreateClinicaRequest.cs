a2b6c85626e69b685679deb83ed383281e02b853
Enrique Chan <enrique.nec@gmail.com>
2022-10-07 11:30:38 -0500

    Update CreateClinicaRequest.cs

--- rakoona.webapiapplication/Entities/Dtos/Request/CreateClinicaRequest.cs
+++ rakoona.webapiapplication/Entities/Dtos/Request/CreateClinicaRequest.cs
@@ -2,7 +2,6 @@
 {
     public class CreateClinicaRequest
     {
-        public DateTime FechaDeCreacion { get; set; }
         public string Nombre { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
