334f4cfc84c3a787cb8f2dba0fe768d1ef3cf331
Enrique Chan <enrique.nec@gmail.com>
2022-10-11 14:40:19 -0500

    Delete AppSettings.cs

--- rakoona.webapiapplication/Configuration/Helpers/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace rakoona.webapiapplication.Configuration.Helpers
-{
-    public class AppSettings
-    {
-        private string secret;
-
-        public string Secret { get => secret; set => secret = value; }
-    }
-}
