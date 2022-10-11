e57fcead60c5e67570272aa3605f4dd6f20c36ac
Enrique Chan <enrique.nec@gmail.com>
2022-10-11 14:40:13 -0500

    Create AppSettings.cs

---
+++ rakoona.webapiapplication/Configuration/Models/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace rakoona.webapi.Configuration.Models
+{
+    public class AppSettings
+    {
+        private string secret;
+
+        public string Secret { get => secret; set => secret = value; }
+    }
+}
