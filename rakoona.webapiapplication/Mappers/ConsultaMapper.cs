b2095896cbf746adb8569aac8a16c86bf0c562f2
Enrique Chan <enrique.nec@gmail.com>
2022-10-11 14:39:52 -0500

    Create ConsultaMapper.cs

---
+++ rakoona.webapiapplication/Mappers/ConsultaMapper.cs
@@ -0,0 +1,29 @@
+﻿using rakoona.webapiapplication.Entities.Dtos.Request;
+using rakoona.webapiapplication.Entities.Dtos.Response;
+using rakoona.webapiapplication.Entities.Models.Consultas;
+
+namespace rakoona.webapiapplication.Mappers
+{
+    public static class ConsultaMapper
+    {
+        public static ConsultaBase CreateFromRequest(this CreateConsultaRequest request, string userId)
+        {
+            ConsultaBase Consulta = new ConsultaBase
+            {
+                ExternalId = Guid.NewGuid().ToString(),
+                FechaDeCreacion = DateTime.Now
+            };
+            return Consulta;
+        }
+
+        public static ConsultaResponse MapToResponse(this ConsultaBase entity)
+        {
+            ConsultaResponse response = new ConsultaResponse
+            {
+                Id = entity.ExternalId,
+                FechaDeCreacion = entity.FechaDeCreacion,
+            };
+            return response;
+        }
+    }
+}
