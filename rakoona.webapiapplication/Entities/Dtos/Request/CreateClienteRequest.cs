04c02aeedb69726614f7094b778d4e6a3e8cb323
Enrique Chan <enrique.nec@gmail.com>
2022-10-12 23:11:14 -0500

    Create CreateClienteRequest.cs

---
+++ rakoona.webapiapplication/Entities/Dtos/Request/CreateClienteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace rakoona.webapiapplication.Entities.Dtos.Request
+{
+    public class CreateClienteRequest
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
