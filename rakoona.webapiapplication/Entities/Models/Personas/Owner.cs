d3441eebd60f0daaaaa02ee9e5504205691784fe
Enrique Chan <enrique.nec@gmail.com>
2022-10-12 23:10:55 -0500

    Delete Owner.cs

--- rakoona.webapiapplication/Entities/Models/Personas/Owner.cs
+++
@@ -1,9 +0,0 @@
-﻿using rakoona.webapiapplication.Entities.Models.Pacientes;
-
-namespace rakoona.webapiapplication.Entities.Models.Personas
-{
-    public class Owner : PersonaBase
-    {
-        public List<Mascota> Mascotas { get; set; }
-    }
-}
