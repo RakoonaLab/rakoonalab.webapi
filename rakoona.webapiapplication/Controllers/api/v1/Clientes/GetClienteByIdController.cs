cabc8ea279cdff3c20a3bbba7556bb3b69acba34
Enrique Chan <enrique.nec@gmail.com>
2022-10-12 23:11:37 -0500

    Create GetClienteByIdController.cs

---
+++ rakoona.webapiapplication/Controllers/api/v1/Clientes/GetClienteByIdController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.webapiapplication.Context;
+using rakoona.webapiapplication.Entities.Models.Personas;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapiapplication.Controllers.api.v1.Clientes
+{
+    [Route("api/clientes")]
+    [Authorize]
+    [ApiController]
+    public class GetClienteByIdController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetClienteByIdController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clientes/5
+        [HttpGet("{id}")]
+        [SwaggerOperation(Tags = new[] { "Clientes" })]
+        public async Task<ActionResult<Cliente>> GetCliente(int id)
+        {
+            if (_context.Clientes == null)
+            {
+                return NotFound();
+            }
+            var cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return cliente;
+        }
+
+    }
+}
