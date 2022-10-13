2dfd31247fed957e52a7b4026c02c8cd28ae5a35
Enrique Chan <enrique.nec@gmail.com>
2022-10-12 23:11:10 -0500

    Create ClienteMapper.cs

---
+++ rakoona.webapiapplication/Mappers/ClienteMapper.cs
@@ -0,0 +1,29 @@
+﻿using rakoona.webapiapplication.Entities.Dtos.Request;
+using rakoona.webapiapplication.Entities.Dtos.Response;
+using rakoona.webapiapplication.Entities.Models.Personas;
+
+namespace rakoona.webapiapplication.Mappers
+{
+    public static class ClienteMapper
+    {
+        public static PersonaBase CreateFromRequest(this CreateClienteRequest request, string userId)
+        {
+            PersonaBase Cliente = new PersonaBase
+            {
+                ExternalId = Guid.NewGuid().ToString(),
+                FechaDeCreacion = DateTime.Now
+            };
+            return Cliente;
+        }
+
+        public static ClienteResponse MapToResponse(this PersonaBase entity)
+        {
+            ClienteResponse response = new ClienteResponse
+            {
+                Id = entity.ExternalId,
+                FechaDeCreacion = entity.FechaDeCreacion,
+            };
+            return response;
+        }
+    }
+}
