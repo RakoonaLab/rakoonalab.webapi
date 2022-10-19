ce59f22626156e07a6c711150eda2877f44e016c
Enrique Chan <enrique.nec@gmail.com>
2022-10-19 11:04:42 -0500

    Update MedicoMapper.cs

--- rakoona.webapiapplication/Mappers/MedicoMapper.cs
+++ rakoona.webapiapplication/Mappers/MedicoMapper.cs
@@ -1,4 +1,5 @@
-﻿using rakoona.webapiapplication.Entities.Dtos.Request;
+﻿using rakoona.webapi.Entities.Models;
+using rakoona.webapiapplication.Entities.Dtos.Request;
 using rakoona.webapiapplication.Entities.Dtos.Response;
 using rakoona.webapiapplication.Entities.Models.Personas;
 
@@ -6,12 +7,25 @@ namespace rakoona.webapiapplication.Mappers
 {
     public static class MedicoMapper
     {
-        public static Medico CreateFromRequest(this CreateMedicoRequest request, string userId)
+        public static Medico CreateFromRequest(this CreateMedicoRequest request, int clinicaId)
         {
+            ClinicaMedico clinicaMedico = new ClinicaMedico
+            {
+                ClinicaId = clinicaId
+            };
+
             Medico medico = new Medico
             {
                 ExternalId = Guid.NewGuid().ToString(),
-                FechaDeCreacion = DateTime.Now
+                FechaDeCreacion = DateTime.Now,
+                PrimerNombre = request.PrimerNombre,
+                SegundoNombre = request.SegundoNombre,
+                PrimerApellido = request.PrimerApellido,
+                SegundoApellido = request.SegundoApellido,
+
+                Nacimiento = request.Nacimiento != "" ? DateTime.Parse(request.Nacimiento) : null,
+
+                ClinicaMedicos = new List<ClinicaMedico> { clinicaMedico }
             };
             return medico;
         }
