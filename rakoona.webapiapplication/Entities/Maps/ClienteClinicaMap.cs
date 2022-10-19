5c469addb425bc2542b869c1af543a078ab1eb99
Enrique Chan <enrique.nec@gmail.com>
2022-10-19 11:04:13 -0500

    Create ClienteClinicaMap.cs

---
+++ rakoona.webapiapplication/Entities/Maps/ClienteClinicaMap.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using rakoona.webapi.Entities.Models;
+using rakoona.webapiapplication.Entities.Models;
+using System.Reflection.Emit;
+
+namespace rakoona.webapiapplication.Entities.Maps
+{
+    public class ClienteClinicaMap : IEntityTypeConfiguration<ClienteClinica>
+    {
+        public void Configure(EntityTypeBuilder<ClienteClinica> builder)
+        {
+            builder.ToTable(name: "ClienteClinica");
+
+            #region Property 
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasColumnName("Id");
+            builder.Property(c => c.ClinicaId).HasColumnName("ClinicaId");
+            builder.Property(c => c.ClienteId).HasColumnName("ClienteId");
+
+            #endregion
+
+            #region Discriminator
+
+            #endregion
+
+            #region HasOne
+
+            builder.HasOne(bc => bc.Clinica)
+                .WithMany(c => c.ClienteClinicas)
+                .HasForeignKey(bc => bc.ClinicaId);
+
+            builder.HasOne(bc => bc.Cliente)
+                .WithMany(c => c.ClienteClinicas)
+                .HasForeignKey(bc => bc.ClienteId);
+            #endregion
+        }
+
+    }
+}
