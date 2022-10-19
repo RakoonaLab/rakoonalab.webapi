dfb2a4df82e801845e4cdc36dbbafdd68bd20d53
Enrique Chan <enrique.nec@gmail.com>
2022-10-19 11:04:39 -0500

    Update ClienteMapper.cs

--- rakoona.webapiapplication/Mappers/ClienteMapper.cs
+++ rakoona.webapiapplication/Mappers/ClienteMapper.cs
@@ -1,4 +1,5 @@
-﻿using rakoona.webapiapplication.Entities.Dtos.Request;
+﻿using rakoona.webapi.Entities.Models;
+using rakoona.webapiapplication.Entities.Dtos.Request;
 using rakoona.webapiapplication.Entities.Dtos.Response;
 using rakoona.webapiapplication.Entities.Models.Personas;
 
@@ -6,12 +7,19 @@ namespace rakoona.webapiapplication.Mappers
 {
     public static class ClienteMapper
     {
-        public static Cliente CreateFromRequest(this CreateClienteRequest request, string userId)
+        public static Cliente CreateFromRequest(this CreateClienteRequest request,int clinicaId)
         {
+            ClienteClinica clienteClinica = new ClienteClinica {
+                 ClinicaId = clinicaId,
+            };
+
             Cliente Cliente = new Cliente
             {
                 ExternalId = Guid.NewGuid().ToString(),
-                FechaDeCreacion = DateTime.Now
+                FechaDeCreacion = DateTime.Now,
+                ClienteClinicas = new List<ClienteClinica> {
+                    clienteClinica
+                } 
             };
             return Cliente;
         }
