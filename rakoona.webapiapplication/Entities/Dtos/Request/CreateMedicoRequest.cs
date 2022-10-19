0766551e222b09a976ff92ef27229fe6cad9d4ad
Enrique Chan <enrique.nec@gmail.com>
2022-10-19 11:00:38 -0500

    Update CreateMedicoRequest.cs

--- rakoona.webapiapplication/Entities/Dtos/Request/CreateMedicoRequest.cs
+++ rakoona.webapiapplication/Entities/Dtos/Request/CreateMedicoRequest.cs
@@ -2,8 +2,10 @@
 {
     public class CreateMedicoRequest
     {
-        public string Nombre { get; set; }
-        public string Direccion { get; set; }
-        public string Telefono { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Nacimiento { get; set; }
     }
 }
