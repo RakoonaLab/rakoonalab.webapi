1cec4ae60109b3e1472355811254196d89f1d6d6
Enrique Chan <enrique.nec@gmail.com>
2022-10-19 11:00:43 -0500

    Create ClienteClinica.cs

---
+++ rakoona.webapiapplication/Entities/Models/ClienteClinica.cs
@@ -0,0 +1,17 @@
+﻿using rakoona.webapiapplication.Entities.Models;
+using rakoona.webapiapplication.Entities.Models.Personas;
+
+namespace rakoona.webapi.Entities.Models
+{
+    public class ClienteClinica
+    {
+        public int Id { get; internal set; }
+
+        public int ClinicaId { get; set; }
+        public Clinica Clinica { get; set; }
+        
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        
+    }
+}
