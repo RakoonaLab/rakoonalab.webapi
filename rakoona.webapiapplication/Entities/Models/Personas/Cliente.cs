cbd3a1c3ab93a049f2475c98456874736dbe3bc3
Enrique Chan <enrique.nec@gmail.com>
2022-10-31 00:30:11 -0600

    Update Cliente.cs

--- rakoona.webapiapplication/Entities/Models/Personas/Cliente.cs
+++ rakoona.webapiapplication/Entities/Models/Personas/Cliente.cs
@@ -5,7 +5,9 @@ namespace rakoona.webapiapplication.Entities.Models.Personas
 {
     public class Cliente : PersonaBase
     {
+        public string Direccion { get; internal set; }
+
         public List<Mascota> Mascotas { get; set; }
-        public List<ClienteClinica> ClienteClinicas { get;  set; }
+        public List<ClienteClinica> ClienteClinicas { get; set; }
     }
 }
