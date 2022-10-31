86d61b83bb5ffdeac405d72a12a989f06aaa37fa
Enrique Chan <enrique.nec@gmail.com>
2022-10-31 00:29:26 -0600

    Create 20221031055455_newchanges.cs

---
+++ rakoona.webapiapplication/Migrations/20221031055455_newchanges.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace rakoona.webapi.Migrations
+{
+    public partial class newchanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Direccion",
+                table: "Personas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Direccion",
+                table: "Personas");
+        }
+    }
+}
