4360431029a06d5d7a729191d4267d4d1e16abac
Enrique Chan <enrique.nec@gmail.com>
2022-10-31 00:29:54 -0600

    Create Contacto.cs

---
+++ rakoona.webapiapplication/Entities/Models/Contacto.cs
@@ -0,0 +1,10 @@
+﻿namespace rakoona.webapi.Entities.Models
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string ExternalId { get; set; }
+        public string ContactType { get; set; }
+        public string ContactValue { get; set; }
+    }
+}
