c40f5be4bd977bd7b542418ae4f85c394bb4a790
Enrique Chan <enrique.nec@gmail.com>
2022-10-31 13:23:40 -0600

    Update ClienteResponse.cs

--- rakoona.webapiapplication/Entities/Dtos/Response/ClienteResponse.cs
+++ rakoona.webapiapplication/Entities/Dtos/Response/ClienteResponse.cs
@@ -3,7 +3,11 @@
     public class ClienteResponse
     {
         public string Id { get; set; }
-        public string Nombre { get; set; }
+        public string NombreCompleto { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
         public DateTime FechaDeCreacion { get; set; }
