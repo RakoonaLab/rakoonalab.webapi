fd66f5e3f0b8053c02b043cebe050409bd5b900a
Enrique Chan <enrique.nec@gmail.com>
2022-10-31 00:30:05 -0600

    Update MedicoResponse.cs

--- rakoona.webapiapplication/Entities/Dtos/Response/MedicoResponse.cs
+++ rakoona.webapiapplication/Entities/Dtos/Response/MedicoResponse.cs
@@ -7,6 +7,10 @@
         public string Direccion { get; set; }
         public string Telefono { get; set; }
         public DateTime FechaDeCreacion { get; set; }
-        
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Nacimiento { get; set; }
     }
 }
