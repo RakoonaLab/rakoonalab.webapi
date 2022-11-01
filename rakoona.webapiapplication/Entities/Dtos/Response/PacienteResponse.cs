fa96df1159154b480a9134f7d9344c9ed67ff6b0
Enrique Chan <enrique.nec@gmail.com>
2022-10-31 23:27:22 -0600

    Create PacienteResponse.cs

---
+++ rakoona.webapiapplication/Entities/Dtos/Response/PacienteResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace rakoona.webapiapplication.Entities.Dtos.Response
+{
+    public class PacienteResponse
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+        
+    }
+}
