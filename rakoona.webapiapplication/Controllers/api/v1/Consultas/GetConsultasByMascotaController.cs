25afdff44fd0fbd043f3667d28badaeb4443afb9
Enrique Chan <enrique.nec@gmail.com>
2022-12-14 00:05:06 -0600

    Create GetConsultasByMascotaController.cs

---
+++ rakoona.webapiapplication/Controllers/api/v1/Consultas/GetConsultasByMascotaController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.webapiapplication.Configuration.Services;
+using rakoona.webapiapplication.Context;
+using rakoona.webapiapplication.Entities.Dtos.Response;
+using rakoona.webapiapplication.Entities.Models.Personas;
+using rakoona.webapiapplication.Mappers;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapiapplication.Controllers.api.v1.Consultas
+{
+    [Route("api/mascota/{mascotaId}/consultas")]
+    [Authorize]
+    [ApiController]
+    public class GetConsultasByMascotaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private IUserInfoService _userInfo;
+
+        public GetConsultasByMascotaController(
+            ApplicationDbContext context,
+            IUserInfoService userInfo
+            )
+        {
+            _userInfo = userInfo;
+            _context = context;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Consultas","Mascotas" })]
+        public async Task<ActionResult<List<ConsultaResponse>>> Get([FromRoute] string mascotaId)
+        {
+            if (_context.Consulta == null)
+            {
+                return NotFound();
+            }
+
+            var mascota = _context.Mascotas.Single(x => x.ExternalId == mascotaId);
+
+
+            var consultas = _context.Consulta.Where(x => x.MascotaRef == mascota.Id).ToList();
+
+            if (consultas == null)
+            {
+                return NotFound();
+            }
+
+            return consultas.Select(x => x.MapToResponse()).ToList();
+        }
+
+    }
+}
