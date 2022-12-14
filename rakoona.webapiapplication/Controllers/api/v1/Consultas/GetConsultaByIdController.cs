d37449eece413bcc3b59ef632e755dce9de7264d
Enrique Chan <enrique.nec@gmail.com>
2022-12-14 00:05:08 -0600

    Create GetConsultaByIdController.cs

---
+++ rakoona.webapiapplication/Controllers/api/v1/Consultas/GetConsultaByIdController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.webapiapplication.Configuration.Services;
+using rakoona.webapiapplication.Context;
+using rakoona.webapiapplication.Entities.Dtos.Response;
+using rakoona.webapiapplication.Entities.Models.Personas;
+using rakoona.webapiapplication.Mappers;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapiapplication.Controllers.api.v1.Consultas
+{
+    [Route("api/consulta/{consultaId}")]
+    [Authorize]
+    [ApiController]
+    public class GetConsultaByIdController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private IUserInfoService _userInfo;
+
+        public GetConsultaByIdController(
+            ApplicationDbContext context,
+            IUserInfoService userInfo
+            )
+        {
+            _userInfo = userInfo;
+            _context = context;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Consultas" })]
+        public async Task<ActionResult<ConsultaResponse>> Get([FromRoute] string consultaId)
+        {
+            if (_context.Consulta == null)
+            {
+                return NotFound();
+            }
+
+            var consulta = _context.Consulta.FirstOrDefault(x => x.ExternalId == consultaId);
+
+            if (consulta == null)
+            {
+                return NotFound();
+            }
+
+            return consulta.MapToResponse();
+        }
+
+    }
+}
