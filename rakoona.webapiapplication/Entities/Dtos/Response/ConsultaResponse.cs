b3ad3fa4dd53852a0be18ab15b55d91dbda54b92
Enrique Chan <enrique.nec@gmail.com>
2022-12-14 00:04:47 -0600

    Update ConsultaResponse.cs

--- rakoona.webapiapplication/Entities/Dtos/Response/ConsultaResponse.cs
+++ rakoona.webapiapplication/Entities/Dtos/Response/ConsultaResponse.cs
@@ -7,6 +7,15 @@
         public string Direccion { get; set; }
         public string Telefono { get; set; }
         public DateTime FechaDeCreacion { get; set; }
-        
+        public string? Observaciones { get; set; }
+        public string? Diagnostico { get; set; }
+        public string? Motivo { get; set; }
+        public double? Temperatura { get; set; }
+        public int? RitmoCardiaco { get; set; }
+        public double? Peso { get; set; }
+        public int? FrecuenciaRespiratoria { get; set; }
+        public string? CaracteristicasDelPulso { get; set; }
+        public int? Pulso { get; set; }
+        public DateTime Fecha { get; set; }
     }
 }
