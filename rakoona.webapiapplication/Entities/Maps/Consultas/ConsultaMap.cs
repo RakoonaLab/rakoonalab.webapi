98aa7d55f1777ca3e0fff35f83382fe845a22902
Enrique Chan <enrique.nec@gmail.com>
2022-12-14 00:04:45 -0600

    Update ConsultaMap.cs

--- rakoona.webapiapplication/Entities/Maps/Consultas/ConsultaMap.cs
+++ rakoona.webapiapplication/Entities/Maps/Consultas/ConsultaMap.cs
@@ -16,6 +16,13 @@ namespace rakoona.webapiapplication.Entities.Maps.Consultas
             builder.Property(c => c.ExternalId).HasColumnName("ExternalId").HasMaxLength(250);
             builder.Property(c => c.FechaDeCreacion).HasColumnName("FechaDeCreacion");
 
+            builder.Property(c => c.Peso).HasColumnName("Peso");
+            builder.Property(c => c.Temperatura).HasColumnName("Temperatura");
+            builder.Property(c => c.FrecuenciaRespiratoria).HasColumnName("FrecuenciaRespiratoria");
+            builder.Property(c => c.Pulso).HasColumnName("Pulso");
+            builder.Property(c => c.CaracteristicasDelPulso).HasColumnName("CaracteristicasDelPulso");
+            builder.Property(c => c.Motivo).HasColumnName("Motivo");
+
             builder.Property(c => c.Observaciones).HasColumnName("Observaciones");
             builder.Property(c => c.Diagnostico).HasColumnName("Diagnostico");
             builder.Property(c => c.Fecha).HasColumnName("Fecha");
