218711638621489b065afc2e7840b21460cbdb98
Enrique Chan <enrique.nec@gmail.com>
2022-12-14 00:04:40 -0600

    Create ConsultaSistemaMusculoEsqueletico.cs

---
+++ rakoona.webapiapplication/Entities/Models/Consultas/ConsultaSistemaMusculoEsqueletico.cs
@@ -0,0 +1,11 @@
+﻿using rakoona.webapiapplication.Entities.Models.Pacientes;
+using rakoona.webapiapplication.Entities.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace rakoona.webapi.Entities.Models.Consultas
+{
+    public class ConsultaSistemaMusculoEsqueletico
+    {
+
+    }
+}
