100c14251b38154ab4ffbd9e35854feabe67edda
Enrique Chan <enrique.nec@gmail.com>
2022-12-19 17:01:12 -0600

    Update CreateVacunaRequest.cs

--- rakoona.dtos/Dtos/Request/CreateVacunaRequest.cs
+++ rakoona.dtos/Dtos/Request/CreateVacunaRequest.cs
@@ -1,11 +1,15 @@
-﻿using Newtonsoft.Json;
-using System.ComponentModel.DataAnnotations;
-
-namespace rakoona.services.Dtos.Request
+﻿namespace rakoona.services.Dtos.Request
 {
     public class CreateVacunaRequest
     {
-        [JsonProperty("nombre", Required = Required.Always)]
         public string? Nombre { get; set; }
+        public int? Pulso { get; set; }
+        public string? CaracteristicasDelPulso { get; set; }
+        public int? FrecuenciaRespiratoria { get; set; }
+        public double? Peso { get; set; }
+        public int? RitmoCardiaco { get; set; }
+        public double? Temperatura { get; set; }
+        public string? Observaciones { get; set; }
+        public string? Lote { get; set; }
     }
 }
