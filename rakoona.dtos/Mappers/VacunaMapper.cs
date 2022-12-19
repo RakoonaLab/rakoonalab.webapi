7b581cb51f15f3955b9cf18d0ef17c0c6392c6d6
Enrique Chan <enrique.nec@gmail.com>
2022-12-19 17:01:22 -0600

    Update VacunaMapper.cs

--- rakoona.dtos/Mappers/VacunaMapper.cs
+++ rakoona.dtos/Mappers/VacunaMapper.cs
@@ -1,20 +1,40 @@
 ﻿using rakoona.services.Dtos.Request;
 using rakoona.services.Dtos.Response;
 using rakoona.services.Entities.Models;
+using rakoona.services.Entities.Models.Consultas;
 using System.Text;
 
 namespace rakoona.services.Mappers
 {
     public static class VacunaMapper
     {
-        public static Vacuna CreateFromRequest(this CreateVacunaRequest request, int? mascotaId)
+        public static Vacuna CreateFromRequest(this CreateVacunaRequest request, int mascotaId)
         {
+            var now = DateTime.Now;
+
+            ConsultaBase Consulta = new ConsultaBase
+            {
+                ExternalId = Guid.NewGuid().ToString(),
+                FechaDeCreacion = now,
+                Fecha = now,
+                Pulso = request.Pulso,
+                CaracteristicasDelPulso = request.CaracteristicasDelPulso,
+                FrecuenciaRespiratoria = request.FrecuenciaRespiratoria,
+                Peso = request.Peso,
+                RitmoCardiaco = request.RitmoCardiaco,
+                Temperatura = request.Temperatura,
+                Motivo = "Vacuna",
+                Observaciones = request.Observaciones,
+                MascotaRef = mascotaId
+            };
+
             Vacuna vacuna = new Vacuna
             {
                 Nombre = request.Nombre,
+                Lote = request.Lote,
                 ExternalId = Guid.NewGuid().ToString(),
-                FechaDeCreacion = DateTime.Now,
-                 MascotaRef = mascotaId.Value
+                FechaDeCreacion = now,
+                Consulta = Consulta
             };
 
 
