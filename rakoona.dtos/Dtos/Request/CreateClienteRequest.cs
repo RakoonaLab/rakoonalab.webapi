07679c0b78059876576ac730407f81fbb124a598
Enrique Chan <enrique.nec@gmail.com>
2022-12-19 17:01:09 -0600

    Update CreateClienteRequest.cs

--- rakoona.dtos/Dtos/Request/CreateClienteRequest.cs
+++ rakoona.dtos/Dtos/Request/CreateClienteRequest.cs
@@ -2,12 +2,12 @@
 {
     public class CreateClienteRequest
     {
-        public string PrimerNombre { get; set; }
-        public string SegundoNombre { get; set; }
-        public string PrimerApellido { get; set; }
-        public string SegundoApellido { get; set; }
+        public string? PrimerNombre { get; set; }
+        public string? SegundoNombre { get; set; }
+        public string? PrimerApellido { get; set; }
+        public string? SegundoApellido { get; set; }
 
-        public string Direccion { get; set; }
-        public string Telefono { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
     }
 }
