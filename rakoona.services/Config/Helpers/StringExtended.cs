2f5e318b3ecb2863c76f475f1e21f243dd965886
Enrique Chan <enrique.nec@gmail.com>
2022-12-28 12:15:36 -0600

    Create StringExtended.cs

---
+++ rakoona.services/Config/Helpers/StringExtended.cs
@@ -0,0 +1,27 @@
+﻿using rakoona.models.dtos.Request;
+using rakoona.services.Entities.Models.Pacientes;
+using rakoona.services.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rakoona.services.Config.Helpers
+{
+    internal static class StringExtended
+    {
+        public static bool IsStringEmpty(this string request)
+        {
+            request = request.Replace(" ", String.Empty);
+            return request == null || request == "" ? true : false;
+        }
+
+        public static int ToNumber(this string request)
+        {
+            int value = Int32.Parse(request);
+            return value;
+        }
+    }
+}
