4946d71f456191235e42acaf15de63c10a197416
Enrique Chan <enrique.nec@gmail.com>
2022-12-28 10:42:42 -0600

    Update Vacuna.cs

--- rakoona.services/Entities/Models/Vacuna.cs
+++ rakoona.services/Entities/Models/Vacuna.cs
@@ -5,9 +5,9 @@ namespace rakoona.services.Entities.Models
     public class Vacuna : ModelBase
     {
         public string Nombre { get; set; }
-        public string Lote { get; set; }
-        public string Laboratorio { get; set; }
-        public DateOnly Caducidad { get; set; }
+        public string? Lote { get; set; }
+        public string? Laboratorio { get; set; }
+        public DateTime? Caducidad { get; set; }
 
         public int ConsultaRef { get; set; }
         public virtual ConsultaPreventiva ConsultaPreventiva { get; set; }
