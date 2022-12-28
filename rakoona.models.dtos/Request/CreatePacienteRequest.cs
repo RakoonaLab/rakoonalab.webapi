e8c49b164540b5e782b50ccdeba26f3c46a145dd
Enrique Chan <enrique.nec@gmail.com>
2022-12-28 12:15:41 -0600

    Update CreatePacienteRequest.cs

--- rakoona.models.dtos/Request/CreatePacienteRequest.cs
+++ rakoona.models.dtos/Request/CreatePacienteRequest.cs
@@ -4,9 +4,9 @@
     {
         public string Nombre { get; set; }
         public string? Genero { get; set; }
-        public int? DiaNacimiento { get; set; }
-        public int? MesNacimiento { get; set; }
-        public int? AnioNacimiento { get; set; }
+        public string? DiaNacimiento { get; set; }
+        public string? MesNacimiento { get; set; }
+        public string? AnioNacimiento { get; set; }
         public string? Especie { get; set; }
         public string? Raza { get; set; }
     }
