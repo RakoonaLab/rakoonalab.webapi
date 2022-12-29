429ab5fc9bd13bbfdf895e6a340df6f69b86b295
Enrique Chan <enrique.nec@gmail.com>
2022-12-29 12:29:24 -0600

    Create informacionDeContactoResponse.cs

---
+++ rakoona.models.dtos/Response/informacionDeContactoResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rakoona.models.dtos.Response
+{
+    public class CelularResponse
+    {
+        public string Id { get; set; }
+        public string Valor { get; set; }
+    }
+}
