d52b62f2262e242e8375337a7204fbd2d3457f7b
Enrique Chan <enrique.nec@gmail.com>
2022-12-29 12:29:16 -0600

    Delete CreateClienteRequest.cs

--- rakoona.models.dtos/Request/CreateClienteRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace rakoona.models.dtos.Request
-{
-    public class CreateClienteRequest
-    {
-        public string? Nombres { get; set; }
-        public string? Apellidos { get; set; }
-        public string? Celular { get; set; }
-    }
-}
