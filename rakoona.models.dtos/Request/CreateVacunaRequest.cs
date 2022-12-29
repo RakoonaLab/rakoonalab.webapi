87fb6fa499cfd7611db4d124a97da29036fe0765
Enrique Chan <enrique.nec@gmail.com>
2022-12-29 12:29:18 -0600

    Update CreateVacunaRequest.cs

--- rakoona.models.dtos/Request/CreateVacunaRequest.cs
+++ rakoona.models.dtos/Request/CreateVacunaRequest.cs
@@ -5,11 +5,10 @@
         
         public double? Peso { get; set; }
         public double? Temperatura { get; set; }
-
         public string? Nombre { get; set; }
         public string? Lote { get; set; }
         public string? Laboratorio { get; set; }
-        public DateTime? Caducidad { get; set; }
-        public string Observaciones { get; set; }
+        public string? Caducidad { get; set; }
+        public string? Observaciones { get; set; }
     }
 }
