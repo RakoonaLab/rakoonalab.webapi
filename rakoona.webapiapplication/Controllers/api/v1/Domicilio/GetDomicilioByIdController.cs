f8a5809d3c8e0521de3d0dc8e754e9ec09054faa
Enrique Chan <enrique.nec@gmail.com>
2022-12-30 11:45:32 -0600

    Create GetDomicilioByIdController.cs

---
+++ rakoona.webapiapplication/Controllers/api/v1/Domicilio/GetDomicilioByIdController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.models.dtos.Response;
+using rakoona.services.Context;
+using rakoona.services.Entities.Mappers;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapi.Controllers.api.v1.Domicilio
+{
+    [Route("api/domicilio/{domicilioId}")]
+    [Authorize]
+    [ApiController]
+    public class GetDomicilioByIdController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetDomicilioByIdController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Domicilio" })]
+        public async Task<ActionResult<DomicilioResponse>> Get(string domicilioId)
+        {
+            var domicilio = _context.Domicilios.Where(x => x.ExternalId == domicilioId && x.Principal).FirstOrDefault();
+
+            if (domicilio == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(domicilio.MapToResponse());
+
+        }
+
+    }
+}
