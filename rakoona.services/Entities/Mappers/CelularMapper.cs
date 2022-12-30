8bbe3e8b9ab57dcf2b82c7e6e36d749cd9fe1f4e
Enrique Chan <enrique.nec@gmail.com>
2022-12-30 11:43:08 -0600

    Create CelularMapper.cs

---
+++ rakoona.services/Entities/Mappers/CelularMapper.cs
@@ -0,0 +1,26 @@
+﻿using rakoona.models.dtos.Request.Clientes;
+using rakoona.models.dtos.Response;
+using rakoona.services.Entities.Models.TiposDeContacto;
+
+namespace rakoona.services.Entities.Mappers
+{
+    public static class CelularMapper
+    {
+        public static CelularResponse MapToResponse(this Contacto entity)
+        {
+            CelularResponse response = new CelularResponse
+            {
+                Id = entity.ExternalId,
+                Valor = entity.Valor
+            };
+            return response;
+        }
+
+        public static Contacto UpdateFromRequest(this UpdateCelularRequest request, Contacto contacto)
+        {
+            contacto.Valor = request.Valor;
+
+            return contacto;
+        }
+    }
+}
