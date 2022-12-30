54d1ac9c81e963461170b937c8a43310f2ff7d1c
Enrique Chan <enrique.nec@gmail.com>
2022-12-30 11:43:37 -0600

    Create UpdateDomicilioRequest.cs

---
+++ rakoona.models.dtos/Request/Domicilio/UpdateDomicilioRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace rakoona.models.dtos.Request.Domicilio
+{
+    public class UpdateDomicilioRequest
+    {
+        public string? Calle { get; set; }
+        public string? Colonia { get; set; }
+        public string? Municipio { get; set; }
+        public string? Estado { get; set; }
+        public string? CP { get; set; }
+    }
+}
