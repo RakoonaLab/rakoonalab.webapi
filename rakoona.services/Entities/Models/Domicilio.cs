064663a56979115de4609c807ec239362b4ec4c8
Enrique Chan <enrique.nec@gmail.com>
2022-12-30 11:43:52 -0600

    Create Domicilio.cs

---
+++ rakoona.services/Entities/Models/Domicilio.cs
@@ -0,0 +1,18 @@
+﻿using rakoona.services.Entities.Models.Personas;
+
+namespace rakoona.services.Entities.Models
+{
+    public class Domicilio : ModelBase
+    {
+        public string Calle { get; set; }
+        public string Colonia { get; set; }
+        public string Municipio { get; set; }
+        public string Estado { get; set; }
+        public string CP { get; set; }
+        public bool Principal { get; set; }
+
+        public int PersonaRef { get; set; }
+        public PersonaBase Persona { get; set; }
+
+    }
+}
