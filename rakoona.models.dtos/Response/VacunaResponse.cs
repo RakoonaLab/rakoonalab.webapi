29c1ffbe37205698c05b01ff89fc6e307d13be4a
Enrique Chan <enrique.nec@gmail.com>
2022-12-30 11:44:11 -0600

    Update VacunaResponse.cs

--- rakoona.models.dtos/Response/VacunaResponse.cs
+++ rakoona.models.dtos/Response/VacunaResponse.cs
@@ -5,6 +5,10 @@
         public string Id { get; set; }
         public string Nombre { get; set; }
         public DateTime FechaDeCreacion { get; set; }
-
+        public string? Laboratorio { get; set; }
+        public DateTime? Caducidad { get; set; }
+        public string? Motivo { get; set; }
+        public string? Observaciones { get; set; }
+        public double? Peso { get; set; }
     }
 }
