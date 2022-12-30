9fd0863834669087103364eaa1e25ad1c70a1492
Enrique Chan <enrique.nec@gmail.com>
2022-12-30 11:43:41 -0600

    Create DomicilioResponse.cs

---
+++ rakoona.models.dtos/Response/DomicilioResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace rakoona.models.dtos.Response
+{
+    public class DomicilioResponse
+    {
+        public string Id { get; set; }
+        public string Calle { get; set; }
+        public string Colonia { get; set; }
+        public string Municipio { get; set; }
+        public string Estado { get; set; }
+        public string CP { get; set; }
+    }
+}
