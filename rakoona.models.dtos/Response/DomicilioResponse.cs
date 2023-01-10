dc7ad51ce0681565e3a3beef9181669ea352fe82
Enrique Chan <enrique.nec@gmail.com>
2023-01-09 23:32:33 -0600

    Update DomicilioResponse.cs

--- rakoona.models.dtos/Response/DomicilioResponse.cs
+++ rakoona.models.dtos/Response/DomicilioResponse.cs
@@ -3,10 +3,11 @@
     public class DomicilioResponse : IResponse
     {
         public string Id { get; set; }
-        public string Calle { get; set; }
-        public string Colonia { get; set; }
-        public string Municipio { get; set; }
-        public string Estado { get; set; }
-        public string CP { get; set; }
+        public string? DomicilioCompleto { get; set; }
+        public string? Calle { get; set; }
+        public string? Colonia { get; set; }
+        public string? Municipio { get; set; }
+        public string? Estado { get; set; }
+        public string? CP { get; set; }
     }
 }
