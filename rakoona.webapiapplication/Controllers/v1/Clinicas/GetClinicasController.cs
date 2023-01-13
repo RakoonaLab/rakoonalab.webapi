1353d993ede1e41900e212bfe5d89fdee71598bc
Enrique Chan <enrique.nec@gmail.com>
2023-01-12 22:21:28 -0600

    Update GetClinicasController.cs

--- rakoona.webapiapplication/Controllers/v1/Clinicas/GetClinicasController.cs
+++ rakoona.webapiapplication/Controllers/v1/Clinicas/GetClinicasController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using rakoona.models.dtos.Response;
-using rakoona.webapiapplication.Configuration.Services;
 using rakoona.services.Context;
 using rakoona.services.Entities.Models;
 using rakoona.services.Entities.Mappers;
 using Swashbuckle.AspNetCore.Annotations;
 using rakoona.services.Entities.Mappers;
+using rakoona.webapi.Services;
 
 namespace rakoona.webapi.Controllers.v1.Clinicas
 {
