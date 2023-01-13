87465bd76cffc0a11901472d1bc48343c469a98a
Enrique Chan <enrique.nec@gmail.com>
2023-01-12 22:20:13 -0600

    Update GetMedicosByClinicaIdController.cs

--- rakoona.webapiapplication/Controllers/v1/Medicos/GetMedicosByClinicaIdController.cs
+++ rakoona.webapiapplication/Controllers/v1/Medicos/GetMedicosByClinicaIdController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using rakoona.models.dtos.Response;
-using rakoona.webapiapplication.Configuration.Services;
 using rakoona.services.Context;
 using rakoona.services.Entities.Mappers;
 using Swashbuckle.AspNetCore.Annotations;
 using rakoona.services.Entities.Mappers;
+using rakoona.webapi.Services;
 
 namespace rakoona.webapi.Controllers.v1.Medicos
 {
