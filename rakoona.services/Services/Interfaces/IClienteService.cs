99bc7a514ef3958ef124affe5d6e2e0630c9ed5a
Enrique Chan <enrique.nec@gmail.com>
2023-01-13 12:50:41 -0600

    Create IClienteService.cs

---
+++ rakoona.services/Services/Interfaces/IClienteService.cs
@@ -0,0 +1,12 @@
+﻿using rakoona.models.dtos.Parameters;
+using rakoona.models.dtos.Response;
+
+namespace rakoona.services.Services.Interfaces
+{
+    public interface IClienteService
+    {
+        Task<List<ClienteResponse>> GetClientesByClinica(string clinicaId,
+                                                        SearchClienteParameters parameters,
+                                                        PaginationParameters pagination);
+    }
+}
