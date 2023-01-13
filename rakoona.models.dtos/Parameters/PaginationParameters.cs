14601cefa992dc27ddbd32c568ac1d507b58bac9
Enrique Chan <enrique.nec@gmail.com>
2023-01-13 12:50:34 -0600

    Create PaginationParameters.cs

---
+++ rakoona.models.dtos/Parameters/PaginationParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace rakoona.models.dtos.Parameters
+{
+    public class PaginationParameters
+    {
+        public PaginationParameters(int? pageSize, int? page)
+        {
+            PageSize = pageSize ?? 10;
+            Page = page ?? 1;
+        }
+
+        public int PageSize { get; }
+        public int Page { get; }
+    }
+}
