c2852c86aff0eb10758c86765bce9f20b7cad10f
Enrique Chan <enrique.nec@gmail.com>
2023-01-19 09:15:24 -0600

    Create IConsultaService.cs

---
+++ rakoona.services/Services/Interfaces/IConsultaService.cs
@@ -0,0 +1,10 @@
+﻿using rakoona.models.dtos.Request.Consultas;
+using rakoona.models.dtos.Response;
+
+namespace rakoona.services.Services.Interfaces
+{
+    public interface IConsultaService
+    {
+        Task<ConsultaResponse> CrearAsync(CreateConsultaRequest request, string mascotaId);
+    }
+}
