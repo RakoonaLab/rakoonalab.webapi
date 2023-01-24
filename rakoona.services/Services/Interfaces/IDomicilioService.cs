c58dbf0c830ea24916500d489c3559cd8fd008af
Enrique Chan <enrique.nec@gmail.com>
2023-01-24 11:45:21 -0600

    Update IDomicilioService.cs

--- rakoona.services/Services/Interfaces/IDomicilioService.cs
+++ rakoona.services/Services/Interfaces/IDomicilioService.cs
@@ -5,9 +5,11 @@ namespace rakoona.services.Services.Interfaces
 {
     public interface IDomicilioService
     {
-        Task<DomicilioResponse> CrearAsync(CreateDomicilioRequest request, string clienteId);
+        Task<DomicilioResponse> CrearByClienteAsync(CreateDomicilioRequest request, string clienteId);
+        Task<DomicilioResponse> CrearByMascotaAsync(CreateDomicilioRequest request, string mascotaId);
         Task<DomicilioResponse?> GetDomicilioAsync(string domicilioId);
         Task<DomicilioResponse?> GetDomicilioPrincipalByClienteAsync(string clienteId);
+        Task<DomicilioResponse?> GetDomicilioPrincipalByMascotaAsync(string mascotaId);
         Task<DomicilioResponse?> ActualizarAsync(UpdateDomicilioRequest request, string domicilioId);
     }
 }
