91212c1fe3b746239a44b9e141c0303e47d40fb4
Enrique Chan <enrique.nec@gmail.com>
2023-01-24 11:45:34 -0600

    Create CreateDomicilioByClienteController.cs

---
+++ rakoona.webapiapplication/Controllers/v1/Domicilio/CreateDomicilioByClienteController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.models.dtos.Request.Domicilio;
+using rakoona.models.dtos.Response;
+using rakoona.services.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+using Microsoft.AspNetCore.Http;
+
+namespace rakoona.webapi.Controllers.v1.Domicilio
+{
+    [Route("api/cliente/{clienteId}/domicilio")]
+    [Authorize]
+    [ApiController]
+    public class CreateDomicilioByClienteController : ControllerBase
+    {
+        private readonly IDomicilioService _domicilioService;
+
+        public CreateDomicilioByClienteController(IDomicilioService domicilioService)
+        {
+            _domicilioService = domicilioService;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Tags = new[] { "Clientes", "Domicilio" })]
+        [ProducesResponseType(typeof(DomicilioResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<DomicilioResponse>> Get([FromBody] CreateDomicilioRequest request, [FromRoute] string clienteId)
+        {
+            var domicilio = await _domicilioService.CrearByClienteAsync(request, clienteId);
+
+            if (domicilio == null)
+                return NotFound();
+
+            return StatusCode(StatusCodes.Status201Created, domicilio);
+        }
+
+    }
+}
