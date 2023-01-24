35ece089551a137993b85e0f060e9053caa23041
Enrique Chan <enrique.nec@gmail.com>
2023-01-24 11:45:45 -0600

    Create GetDomicilioByMascota.cs

---
+++ rakoona.webapiapplication/Controllers/v1/Domicilio/GetDomicilioByMascota.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.models.dtos.Response;
+using rakoona.services.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapi.Controllers.v1.Domicilio
+{
+    [Route("api/mascota/{mascotaId}/domicilio")]
+    [Authorize]
+    [ApiController]
+    public class GetDomicilioByMascotaController : ControllerBase
+    {
+        private readonly IDomicilioService _domicilioService;
+
+        public GetDomicilioByMascotaController(IDomicilioService domicilioService)
+        {
+            _domicilioService = domicilioService;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Mascotas", "Domicilio" })]
+        public async Task<ActionResult<DomicilioResponse>> Get(string mascotaId)
+        {
+            var domicilio = await _domicilioService.GetDomicilioPrincipalByMascotaAsync(mascotaId);
+
+            if (domicilio == null)
+                return NotFound();
+
+            return Ok(domicilio);
+        }
+
+    }
+}
