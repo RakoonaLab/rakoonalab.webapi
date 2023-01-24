143d579a4605cb8ab67902ed22f2d2a4ea3c404b
Enrique Chan <enrique.nec@gmail.com>
2023-01-24 11:45:30 -0600

    Create DeleteConsultaController.cs

---
+++ rakoona.webapiapplication/Controllers/v1/Consultas/DeleteConsultaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.models.dtos.Response;
+using rakoona.services.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapi.Controllers.v1.Consultas
+{
+    [Route("api/consulta/{consultaId}")]
+    [Authorize]
+    [ApiController]
+    public class DeleteConsultaController : ControllerBase
+    {
+        private readonly IConsultaService _consultaService;
+
+        public DeleteConsultaController(
+            IConsultaService consultaService)
+        {
+            _consultaService= consultaService;
+        }
+
+        [HttpDelete]
+        [SwaggerOperation(Tags = new[] { "Consultas" })]
+        public async Task<ActionResult<ConsultaResponse>> Delete([FromRoute] string consultaId)
+        {
+            var delete = await _consultaService.DeleteAsync(consultaId);
+
+            if (delete)
+            {
+                return StatusCode(StatusCodes.Status200OK);
+            }
+
+            return Problem();
+        }
+
+    }
+}
