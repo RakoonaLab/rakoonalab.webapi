bf92e66911b7fe6af0a9cbefbb9c07512c917a01
Enrique Chan <enrique.nec@gmail.com>
2023-01-24 11:45:42 -0600

    Create CreateDomicilioByMascotaController.cs

---
+++ rakoona.webapiapplication/Controllers/v1/Domicilio/CreateDomicilioByMascotaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.models.dtos.Request.Domicilio;
+using rakoona.models.dtos.Response;
+using rakoona.services.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+using Microsoft.AspNetCore.Http;
+
+namespace rakoona.webapi.Controllers.v1.Domicilio
+{
+    [Route("api/mascota/{mascotaId}/domicilio")]
+    [Authorize]
+    [ApiController]
+    public class CreateDomicilioByMascotaController : ControllerBase
+    {
+        private readonly IDomicilioService _domicilioService;
+
+        public CreateDomicilioByMascotaController(IDomicilioService domicilioService)
+        {
+            _domicilioService = domicilioService;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Tags = new[] { "Mascotas", "Domicilio" })]
+        [ProducesResponseType(typeof(DomicilioResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<DomicilioResponse>> Get([FromBody] CreateDomicilioRequest request, [FromRoute] string mascotaId)
+        {
+            var domicilio = await _domicilioService.CrearByMascotaAsync(request, mascotaId);
+
+            if (domicilio == null)
+                return NotFound();
+
+            return StatusCode(StatusCodes.Status201Created, domicilio);
+        }
+
+    }
+}
