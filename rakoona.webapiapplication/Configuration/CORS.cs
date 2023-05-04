fbbddf0a43f9364f29efe5057f3fb65bf3a59ed8
Enrique Chan <enrique.nec@gmail.com>
2023-05-04 12:07:22 -0600

    Create CORS.cs

---
+++ rakoona.webapiapplication/Configuration/CORS.cs
@@ -0,0 +1,17 @@
+﻿namespace rakoona.webapi.Configuration
+{
+    public class CORS
+    {
+        public static void Config(ref WebApplicationBuilder builder) {
+            builder.Services.AddCors(options => options.AddPolicy("AllowOrigin", builder =>
+            {
+                builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:4200")
+                .AllowCredentials();
+            }));
+
+        }
+    }
+}
