a23423526c76e8dae25e64e32e194589c0879794
Enrique Chan <enrique.nec@gmail.com>
2023-05-08 10:40:56 -0600

    Update DataBase.cs

--- rakoona.webapiapplication/Configuration/DataBase.cs
+++ rakoona.webapiapplication/Configuration/DataBase.cs
@@ -7,9 +7,27 @@ namespace rakoona.webapi.Configuration
     {
         public static void  ConfigConnectionString(ref WebApplicationBuilder builder)
         {
-            var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
-            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+
+                var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
+                builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            }
+            else
+            {
+                var connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+                builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            }
         }
 
+        public static void ApplyMigration(ref WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+        }
     }
 }
