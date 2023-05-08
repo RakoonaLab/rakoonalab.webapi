e1187c50c27396005253fbf759cb0b6b6439f032
Enrique Chan <enrique.nec@gmail.com>
2023-05-08 10:41:01 -0600

    Update UserInfoService.cs

--- rakoona.webapiapplication/Services/UserInfoService.cs
+++ rakoona.webapiapplication/Services/UserInfoService.cs
@@ -18,9 +18,9 @@ namespace rakoona.webapi.Services
         {
             _httpContextAccessor = httpContextAccessor;
 
-            var accessToken = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
+            var authorization = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"];
 
-            var token = accessToken.ToString().Replace("Bearer ", "");
+            var token = authorization.ToString().Replace("Bearer ", "");
 
             var handler = new JwtSecurityTokenHandler();
             var decodedValue = handler.ReadJwtToken(token);
