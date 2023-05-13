c3f56a16e8eafe861ee12cb05553749f9a4cbd23
Enrique Chan <enrique.nec@gmail.com>
2023-05-12 21:57:44 -0600

    Update CORS.cs

--- rakoona.webapiapplication/Configuration/CORS.cs
+++ rakoona.webapiapplication/Configuration/CORS.cs
@@ -3,15 +3,16 @@
     public class CORS
     {
         public static void Config(ref WebApplicationBuilder builder) {
+            var site = builder.Environment.IsDevelopment() ? "http://localhost:4200" : "https://thechanec.github.io/";
+
             builder.Services.AddCors(options => options.AddPolicy("AllowOrigin", builder =>
             {
                 builder
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .WithOrigins("http://localhost:4200")
+                .WithOrigins(site)
                 .AllowCredentials();
             }));
-
         }
     }
 }
