19da0a531ea2a526dd654279e8a15284e29462ef
Enrique Chan <enrique.nec@gmail.com>
2023-05-19 19:54:29 -0600

    Create AplicacionBase.cs

---
+++ rakoona.services/Entities/Models/AplicacionBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rakoona.services.Entities.Models
+{
+    public  class AplicacionBase : ModelBase
+    {
+        public DateTime FechaAplicacion { get; set; }
+    }
+}
