72e1cf7107fdead6aaaebf45566a8328dd4e9262
Enrique Chan <enrique.nec@gmail.com>
2023-05-19 19:54:39 -0600

    Create Pulso.cs

---
+++ rakoona.services/Entities/Models/Consultas/Mediciones/Pulso.cs
@@ -0,0 +1,17 @@
+﻿using rakoona.services.Entities.Models.Consultas;
+using rakoona.services.Entities.Models.Pacientes;
+
+namespace rakoona.services.Entities.Models.Consultas.Mediciones
+{
+    public class Pulso : ModelBase
+    {
+        public DateTime FechaAplicacion { get; set; }
+        public int? Valor { get; set; }
+        public string? Caracteristicaso { get; set; }
+
+
+        public int MascotaRef { get; set; }
+        public Mascota? Mascota { get; set; }
+        public Consulta? Consulta { get; set; }
+    }
+}
