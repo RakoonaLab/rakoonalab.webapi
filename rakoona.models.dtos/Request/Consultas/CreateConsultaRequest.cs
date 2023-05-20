e70aa4b7497531c0f57a0a7373f4717cbb391a1c
Enrique Chan <enrique.nec@gmail.com>
2023-05-19 19:54:01 -0600

    Update CreateConsultaRequest.cs

--- rakoona.models.dtos/Request/Consultas/CreateConsultaRequest.cs
+++ rakoona.models.dtos/Request/Consultas/CreateConsultaRequest.cs
@@ -2,16 +2,12 @@
 {
     public class CreateConsultaRequest
     {
-        public CreateConsultaRequest()
-        {
-
-        }
-
         public int? Pulso { get; set; }
         public int? FrecuenciaRespiratoria { get; set; }
         public double? Peso { get; set; }
         public int? RitmoCardiaco { get; set; }
         public double? Temperatura { get; set; }
+
         public string? Motivo { get; set; }
         public string? Diagnostico { get; set; }
         public string? Observaciones { get; set; }
