df018bb4c146ce59d6c243ebde4a372532483bdf
Enrique Chan <enrique.nec@gmail.com>
2023-05-19 19:54:49 -0600

    Update Medico.cs

--- rakoona.services/Entities/Models/Personas/Medico.cs
+++ rakoona.services/Entities/Models/Personas/Medico.cs
@@ -2,9 +2,12 @@
 
 namespace rakoona.services.Entities.Models.Personas
 {
-    public class Medico : PersonaBase
+    public class Medico : ModelBase
     {
-        public ICollection<ClinicaMedico> ClinicaMedicos { get; set; }
-        public ICollection<Consulta> Consultas { get; set; }
+        public int PersonaRef { get; set; }
+        public PersonaBase? PersonaInfo { get; set; }
+
+        public ICollection<ClinicaMedico>? ClinicaMedicos { get; set; }
+        public ICollection<Consulta>? Consultas { get; set; }
     }
 }
