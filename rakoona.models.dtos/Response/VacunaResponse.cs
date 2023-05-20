647f7b2f08799da06a205676160fd54a66c988ed
Enrique Chan <enrique.nec@gmail.com>
2023-05-19 19:54:07 -0600

    Update VacunaResponse.cs

--- rakoona.models.dtos/Response/VacunaResponse.cs
+++ rakoona.models.dtos/Response/VacunaResponse.cs
@@ -2,22 +2,25 @@
 {
     public class VacunaResponse : IResponse
     {
-        public string Id { get; set; }
-        public string Nombre { get; set; }
-        public DateTime FechaDeCreacion { get; set; }
+        public string? Id { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? FechaDeCreacion { get; set; }
         public string? Laboratorio { get; set; }
         public string? Caducidad { get; set; }
         public string? Motivo { get; set; }
         public string? Observaciones { get; set; }
         public double? Peso { get; set; }
         public string? Lote { get; set; }
-        public double Temperatura { get; set; }
+        public double? Temperatura { get; set; }
         public string? Fecha { get; set; }
         public string? MedicoId { get; set; }
         public string? MedicoNombre { get; set; }
         public string? MascotaId { get; set; }
         public string? MascotaNombre { get; set; }
         public string? DuenioId { get; set; }
-        public string DuenioNombre { get; set; }
+        public string? DuenioNombre { get; set; }
+        public int? Pulso { get; set; }
+        public int? RitmoCardiaco { get; set; }
+        public int? FrecuenciaRespiratoria { get; set; }
     }
 }
