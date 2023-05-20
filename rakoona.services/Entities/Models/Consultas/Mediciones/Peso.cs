02dff1df08c646cb4974613574181c3b9934a924
Enrique Chan <enrique.nec@gmail.com>
2023-05-19 19:54:36 -0600

    Create Peso.cs

---
+++ rakoona.services/Entities/Models/Consultas/Mediciones/Peso.cs
@@ -0,0 +1,16 @@
+﻿using rakoona.services.Entities.Models.Consultas;
+using rakoona.services.Entities.Models.Pacientes;
+
+namespace rakoona.services.Entities.Models.Consultas.Mediciones
+{
+    public class Peso : ModelBase
+    {
+        public DateTime FechaAplicacion { get; set; }
+        public double? Valor { get; set; }
+
+
+        public int MascotaRef { get; set; }
+        public Mascota? Mascota { get; set; }
+        public Consulta? Consulta { get; set; }
+    }
+}
