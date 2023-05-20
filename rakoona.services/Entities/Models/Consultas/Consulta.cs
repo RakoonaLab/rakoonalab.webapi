ab2738198e3f55c2d3d15d29ba32a71373f887a5
Enrique Chan <enrique.nec@gmail.com>
2023-05-19 19:54:31 -0600

    Update Consulta.cs

--- rakoona.services/Entities/Models/Consultas/Consulta.cs
+++ rakoona.services/Entities/Models/Consultas/Consulta.cs
@@ -1,20 +1,13 @@
-﻿using rakoona.services.Entities.Models.Pacientes;
+﻿using rakoona.services.Entities.Models.Consultas.Mediciones;
+using rakoona.services.Entities.Models.Pacientes;
 using rakoona.services.Entities.Models.Personas;
 
 namespace rakoona.services.Entities.Models.Consultas
 {
-    public class Consulta : ModelBase
+    public class Consulta : AplicacionBase
     {
         public Consulta() { }   
 
-        public DateTime FechaAplicacion { get; set; }
-
-        public double? Peso { get; set; }
-        public double? Temperatura { get; set; }
-        public int? RitmoCardiaco { get; set; }
-        public int? FrecuenciaRespiratoria { get; set; }
-        public int? Pulso { get; set; }
-        public string? CaracteristicasDelPulso { get; set; }
         public string? Observaciones { get; set; }
         public string? Motivo { get; set; }
         public string? Diagnostico { get; set; }
@@ -22,8 +15,25 @@ namespace rakoona.services.Entities.Models.Consultas
         public int MedicoRef { get; set; }
         public virtual Medico? Medico { get; set; }
 
+        public int PesoRef { get; set; }
+        public virtual Peso? Peso { get; set; }
+
+        public int TemperaturaRef { get; set; }
+        public virtual Temperatura? Temperatura { get; set; }
+
+        public int PulsoRef { get; set; }
+        public virtual Pulso? Pulso { get; set; }
+
+        public int RitmoCardiacoRef { get; set; }
+        public virtual RitmoCardiaco? RitmoCardiaco { get; set; }
+
+        public int FrecuenciaRespiratoriaRef { get; set; }
+        public virtual FrecuenciaRespiratoria? FrecuenciaRespiratoria { get; set; }
+
         public int MascotaRef { get; set; }
         public virtual Mascota? Mascota { get; set; }
+
+
         public virtual List<Vacunacion>? Vacunas { get; set; }
         
     }
