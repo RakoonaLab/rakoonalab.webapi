a5a7fc5883a98d1b2689155f026c3f080167d0df
Enrique Chan <enrique.nec@gmail.com>
2023-05-22 22:39:08 -0600

    Create GetClinicasByUserController.cs

---
+++ rakoona.webapiapplication/Controllers/v1/Clinicas/GetClinicasByUserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.models.dtos.Response;
+using rakoona.services.Services.Interfaces;
+using rakoona.webapi.Services;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapi.Controllers.v1.Clinicas
+{
+    [Route("api/clinicas")]
+    [Authorize]
+    [ApiController]
+    public class GetClinicasByUserController : ControllerBase
+    {
+        private readonly IClinicaService _context;
+        private IUserInfoService _userInfo;
+
+        public GetClinicasByUserController(
+            IClinicaService context,
+            IUserInfoService userInfo
+            )
+        {
+            _userInfo = userInfo;
+            _context = context;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Clinica" })]
+        public async Task<ActionResult<List<ClinicaResponse>>> Get()
+        {
+            var clinicas = await _context.GetByUser(_userInfo.UserId);
+
+            if (clinicas == null)
+                return NoContent();
+            
+            return Ok(clinicas);
+        }
+
+    }
+}
