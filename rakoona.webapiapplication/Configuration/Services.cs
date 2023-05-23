f2d679ad6b1fab9e62de3205398e3f058b2a417e
Enrique Chan <enrique.nec@gmail.com>
2023-05-22 22:39:05 -0600

    Update Services.cs

--- rakoona.webapiapplication/Configuration/Services.cs
+++ rakoona.webapiapplication/Configuration/Services.cs
@@ -8,17 +8,16 @@ namespace rakoona.webapi.Configuration
     {
         public static void ConfigDependencyInjention(ref WebApplicationBuilder builder)
         {
-            builder.Services.AddTransient<IUserInfoService, UserInfoService>();
-            builder.Services.AddTransient<IDomicilioService, DomicilioService>();
-            builder.Services.AddTransient<IMascotaService, MascotaService>();
-            builder.Services.AddTransient<IVacunaService, VacunaService>();
             builder.Services.AddTransient<IClienteService, ClienteService>();
-            builder.Services.AddTransient<IConsultaService, ConsultaService>();
-            builder.Services.AddTransient<IColoresPorMascotaService, ColoresPorMascotaService>();
-            builder.Services.AddTransient<IMedicoService, MedicoService>();
             builder.Services.AddTransient<IClinicaService, ClinicaService>();
-            
+            builder.Services.AddTransient<IColoresPorMascotaService, ColoresPorMascotaService>();
+            builder.Services.AddTransient<IConsultaService, ConsultaService>();
+            builder.Services.AddTransient<IDomicilioService, DomicilioService>();
+            builder.Services.AddTransient<IInformacionDeContactoService, InformacionDeContactoService>();
+            builder.Services.AddTransient<IMascotaService, MascotaService>();
+            builder.Services.AddTransient<IMedicoService, MedicoService>();
+            builder.Services.AddTransient<IVacunaService, VacunaService>();
+            builder.Services.AddTransient<IUserInfoService, UserInfoService>();
         }
-
     }
 }
