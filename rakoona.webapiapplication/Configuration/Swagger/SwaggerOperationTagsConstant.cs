5c278214bc57275796da3acd60927fd308f5b37e
Enrique Chan <enrique.nec@gmail.com>
2023-05-26 14:31:55 -0600

    Create SwaggerOperationTagsConstant.cs

---
+++ rakoona.webapiapplication/Configuration/Swagger/SwaggerOperationTagsConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace rakoona.webapi.Configuration.Swagger
+{
+    internal class SwaggerOperationTagsConstant
+    {
+        internal const string Mascotas = "Mascotas";
+        internal const string Citas = "Citas";
+        internal const string Clinica = "Clinica";
+        internal const string Vacunas = "Vacunas";
+    }
+}
