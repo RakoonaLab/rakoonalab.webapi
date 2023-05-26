b5b33b35a76fbbb7c9107a6cf77f73b21bc57421
Enrique Chan <enrique.nec@gmail.com>
2023-05-26 14:14:45 -0600

    Create ICitaService.cs

---
+++ rakoona.services/Services/Interfaces/ICitaService.cs
@@ -0,0 +1,10 @@
+﻿using rakoona.models.dtos.Request;
+using rakoona.models.dtos.Response;
+
+namespace rakoona.services.Services.Interfaces
+{
+    public interface ICitaService
+    {
+        Task<CitaResponse> Create(CreateCitaRequest request, string mascotaId);
+    }
+}
