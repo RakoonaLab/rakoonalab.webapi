def522b72f3eff7ef245129e892618898a5f0fd0
Enrique Chan <enrique.nec@gmail.com>
2023-05-26 14:31:52 -0600

    Create Swagger.cs

---
+++ rakoona.webapiapplication/Configuration/Swagger/Swagger.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace rakoona.webapi.Configuration.Swagger
+{
+    public class Swagger
+    {
+        public static void Config(ref WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Rakoona-APIs",
+                    Description = "Rakoona-APIs.",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+                options.EnableAnnotations();
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+            });
+        }
+    }
+}
