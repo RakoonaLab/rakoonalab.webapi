0494c425177b12119bcf1912a6294b7e98a9e7a6
Enrique Chan <enrique.nec@gmail.com>
2023-05-26 14:14:56 -0600

    Create CitaMap.cs

---
+++ rakoona.services/Entities/Maps/Consultas/CitaMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using rakoona.services.Entities.Models.Consultas;
+
+namespace rakoona.services.Entities.Maps.Consultas
+{
+    internal class CitaMap : IEntityTypeConfiguration<Cita>
+    {
+        public void Configure(EntityTypeBuilder<Cita> builder)
+        {
+            builder.ToTable(name: "Citas");
+
+            #region Property 
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasColumnName("Id");
+            builder.Property(c => c.ExternalId).HasColumnName("ExternalId").HasMaxLength(250);
+            builder.Property(c => c.FechaDeCreacion).HasColumnName("FechaDeCreacion");
+            builder.Property(c => c.Fecha).HasColumnName("Fecha");
+            builder.Property(c => c.Comentarios).HasColumnName("Comentarios");
+
+            builder.Property(c => c.MascotaRef).HasColumnName("MascotaRef");
+
+            #endregion
+
+            builder.HasOne(a => a.Mascota)
+                    .WithMany(b => b.Citas)
+                    .HasForeignKey(b => b.MascotaRef)
+                    .OnDelete(DeleteBehavior.NoAction);
+        }
+
+    }
+}
