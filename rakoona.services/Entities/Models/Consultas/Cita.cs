d71f2f2ebae952447724ab2627d9be4d81c2bd17
Enrique Chan <enrique.nec@gmail.com>
2023-05-26 14:14:58 -0600

    Create Cita.cs

---
+++ rakoona.services/Entities/Models/Consultas/Cita.cs
@@ -0,0 +1,12 @@
+﻿using rakoona.services.Entities.Models.Pacientes;
+
+namespace rakoona.services.Entities.Models.Consultas
+{
+    public class Cita : ModelBase
+    {
+        public DateTime Fecha  { get; set; }
+        public string Comentarios { get; set; }
+        public int MascotaRef { get; set; }
+        public virtual Mascota? Mascota { get; set; }
+    }
+}
