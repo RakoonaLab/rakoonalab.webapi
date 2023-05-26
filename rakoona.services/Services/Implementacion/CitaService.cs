6b353f2d411c7cb4d8446cc7192f3ab4ab1a4bc7
Enrique Chan <enrique.nec@gmail.com>
2023-05-26 14:15:03 -0600

    Create CitaService.cs

---
+++ rakoona.services/Services/Implementacion/CitaService.cs
@@ -0,0 +1,14 @@
+﻿using rakoona.models.dtos.Request;
+using rakoona.models.dtos.Response;
+using rakoona.services.Services.Interfaces;
+
+namespace rakoona.services.Services.Implementacion
+{
+    public class CitaService : ICitaService
+    {
+        public Task<CitaResponse> Create(CreateCitaRequest request, string mascotaId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
