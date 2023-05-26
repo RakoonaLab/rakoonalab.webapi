4199586ac13462877add6169bb145dff7923a9f2
Enrique Chan <enrique.nec@gmail.com>
2023-05-26 14:14:48 -0600

    Create CreateCitaRequest.cs

---
+++ rakoona.models.dtos/Request/CreateCitaRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace rakoona.models.dtos.Request
+{
+    public class CreateCitaRequest
+    {
+        public DateTime Fecha { get; set; }
+        public string Comentarios { get; set; }
+    }
+}
