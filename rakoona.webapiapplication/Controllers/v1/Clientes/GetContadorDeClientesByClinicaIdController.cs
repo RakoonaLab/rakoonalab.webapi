d3cb8fa2ec651d4a2eeb45315323fd71ae853a99
Enrique Chan <enrique.nec@gmail.com>
2023-06-30 09:22:25 -0600

    Create GetContadorDeClientesByClinicaIdController.cs

---
+++ rakoona.webapiapplication/Controllers/v1/Clientes/GetContadorDeClientesByClinicaIdController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.models.dtos.Response;
+using rakoona.services.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapi.Controllers.v1.Clientes
+{
+    [Route("api/clinica/{clinicaId}/clientes/count")]
+    [Authorize]
+    [ApiController]
+    public class GetContadorDeClientesByClinicaIdController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;
+
+        public GetContadorDeClientesByClinicaIdController(
+            IClienteService clienteService
+            )
+        {
+            _clienteService = clienteService;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Clientes", "Clinica" })]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<List<ClienteResponse>>> Get([FromRoute] string clinicaId)
+        {
+            var result = await _clienteService.GetContadorClientesByClinica(clinicaId);
+
+            return Ok(result);
+        }
+
+    }
+}
