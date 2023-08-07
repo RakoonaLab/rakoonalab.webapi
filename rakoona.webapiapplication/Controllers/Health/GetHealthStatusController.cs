cad4064e0a75211358f5b5c4f17fd77938c0a809
Enrique Chan <enrique.nec@gmail.com>
2023-08-06 18:14:07 -0600

    Create GetHealthStatusController.cs

---
+++ rakoona.webapiapplication/Controllers/Health/GetHealthStatusController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapi.Controllers.Health
+{
+    [Route("api/health")]
+    [Authorize]
+    [ApiController]
+    public class GetHealthStatusController : ControllerBase
+    {
+        public GetHealthStatusController()
+        {
+            
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { 
+            "Health" 
+        })]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public ActionResult Get()
+        {
+            
+
+            return Ok();
+        }
+
+    }
+}
