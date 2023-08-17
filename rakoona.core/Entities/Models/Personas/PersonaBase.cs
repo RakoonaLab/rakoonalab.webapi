76b5276490d69102200eae017d0b7018fc4a63c8
Enrique Chan <enrique.nec@gmail.com>
2023-08-17 14:10:35 -0600

    remove PersonaMap

--- rakoona.core/Entities/Models/Personas/PersonaBase.cs
+++ rakoona.core/Entities/Models/Personas/PersonaBase.cs
@@ -1,4 +1,6 @@
-﻿using rakoona.core.Entities.Models.Seguridad;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using rakoona.core.Entities.Models.Seguridad;
 using rakoona.core.Entities.Models.TiposDeContacto;
 
 namespace rakoona.core.Entities.Models.Personas
@@ -20,5 +22,41 @@ namespace rakoona.core.Entities.Models.Personas
             nombreCompleto += Apellidos != "" ? " " + Apellidos : Apellidos;
             return nombreCompleto;
         }
+    }
+
+    public class PersonaMap : IEntityTypeConfiguration<PersonaBase>
+    {
+        public void Configure(EntityTypeBuilder<PersonaBase> builder)
+        {
+            builder.ToTable(name: "Personas");
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasColumnName("Id");
+            builder.Property(c => c.ExternalId).HasColumnName("ExternalId").HasMaxLength(250);
+            builder.Property(c => c.FechaDeCreacion).HasColumnName("FechaDeCreacion");
+            builder.Property(c => c.Nombres).HasColumnName("Nombres");
+            builder.Property(c => c.Apellidos).HasColumnName("Apellidos");
+            builder.Property(c => c.FechaDeNacimiento).HasColumnName("FechaDeNacimiento");
+
+
+            builder.HasOne(a => a.User)
+                .WithOne(b => b.Persona)
+                .HasForeignKey<PersonaBase>(b => b.UsuarioRef);
+
+            builder.HasMany(a => a.InformacionDeContacto)
+                .WithOne(b => b.Persona)
+                .HasForeignKey(b => b.PersonaRef);
+
+            builder.HasMany(a => a.Domicilios)
+                .WithOne(b => b.Persona)
+                .HasForeignKey(b => b.PersonaRef);
+
+            builder.HasOne(a => a.MedicoInfo)
+                .WithOne(b => b.PersonaInfo)
+                .HasForeignKey<Medico>(b => b.PersonaRef);
+
+
+        }
+
     }
 }
