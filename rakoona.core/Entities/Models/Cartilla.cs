9daa6fb1b9aee62e044f5c7670a703705421bedf
Enrique Chan <enrique.nec@gmail.com>
2023-08-30 21:42:51 -0600

    Update Cartilla.cs

--- rakoona.core/Entities/Models/Cartilla.cs
+++ rakoona.core/Entities/Models/Cartilla.cs
@@ -1,4 +1,6 @@
-﻿using rakoona.core.Entities.Models.Consultas;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using rakoona.core.Entities.Models.Consultas;
 using rakoona.core.Entities.Models.Pacientes;
 
 namespace rakoona.core.Entities.Models
@@ -11,5 +13,23 @@ namespace rakoona.core.Entities.Models
         public virtual List<Consulta>? Consultas { get; set; }
         public virtual List<PlanDeVacunacion>? PlanesDeVacunacion { get; set; }
 
+    }
+
+    public class CartillaMap : IEntityTypeConfiguration<Cartilla>
+    {
+        public void Configure(EntityTypeBuilder<Cartilla> builder)
+        {
+            builder.ToTable(name: "Cartillas");
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasColumnName("Id");
+            builder.Property(c => c.ExternalId).HasColumnName("ExternalId").HasMaxLength(250);
+            builder.Property(c => c.FechaDeCreacion).HasColumnName("FechaDeCreacion");
+            builder.Property(c => c.MascotaRef).HasColumnName("MascotaRef");
+
+            builder.HasOne(a => a.Mascota)
+                   .WithOne(b => b.Cartilla)
+                   .HasForeignKey<Cartilla>(b => b.MascotaRef);
+        }
     }
 }
