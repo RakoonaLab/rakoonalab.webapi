e498109c7afc4ee6893da16b22c6931e13a1cefa
Enrique Chan <enrique.nec@gmail.com>
2023-09-09 15:54:16 -0600

    Create GetPlanesController.cs

---
+++ rakoona.webapiapplication/Controllers/v1/Subscripciones/GetPlanesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rakoona.core.Services.Interfaces;
+using rakoona.dtos.Response;
+using rakoona.webapi.Configuration.Swagger;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace rakoona.webapi.Controllers.v1.Clinicas
+{
+    [Route("api/subscripciones/planes")]
+    [Authorize]
+    [ApiController]
+    public class GetPlanesController : ControllerBase
+    {
+        private readonly IPlanService _context;
+
+        public GetPlanesController(IPlanService context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { SwaggerOperationTagsConstant.Subscripcion })]
+        public async Task<ActionResult<ClinicaResponse>> Get()
+        {
+
+            var planes = await _context.GetPlanes();
+
+            if (planes == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(planes);
+        }
+
+    }
+}
