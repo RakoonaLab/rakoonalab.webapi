6e7f596e0db3ad2921132dd9bd2b78951e466763
Enrique Chan <enrique.nec@gmail.com>
2023-09-19 12:02:56 -0600

    Update PlanesDBInitializer.cs

--- rakoona.core/Context/Seeds/Subscripciones/PlanesDBInitializer.cs
+++ rakoona.core/Context/Seeds/Subscripciones/PlanesDBInitializer.cs
@@ -5,8 +5,8 @@ namespace rakoona.core.Context.Seeds
 {
     public static class PlanesDBInitializer
     {
-        public static Plan Free { get { return new Plan { Id = 1, FechaDeCreacion = DateTime.Now, ExternalId = Guid.NewGuid().ToString(), Nombre = "Free" }; } }
-        public static Plan Basico { get { return new Plan { Id = 2, FechaDeCreacion = DateTime.Now, ExternalId = Guid.NewGuid().ToString(), Nombre = "Basico" }; } }
+        public static Plan Free { get { return new Plan { Id = 1, FechaDeCreacion = DateTime.Now, ExternalId = Guid.NewGuid().ToString(), Nombre = "Free", NumeroDeClinicas = 1, NumeroDeMedicos = 1 }; } }
+        public static Plan Basico { get { return new Plan { Id = 2, FechaDeCreacion = DateTime.Now, ExternalId = Guid.NewGuid().ToString(), Nombre = "Basico", NumeroDeClinicas = 1, NumeroDeMedicos= 5 }; } }
 
         internal static void Seed(ModelBuilder builder)
         {
