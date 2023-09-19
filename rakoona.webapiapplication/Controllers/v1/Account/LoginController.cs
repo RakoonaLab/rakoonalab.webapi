999519a52ca757eb251c30e41d090625765bb36d
Enrique Chan <enrique.nec@gmail.com>
2023-09-19 14:33:05 -0600

    Update LoginController.cs

--- rakoona.webapiapplication/Controllers/v1/Account/LoginController.cs
+++ rakoona.webapiapplication/Controllers/v1/Account/LoginController.cs
@@ -1,87 +1,40 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using rakoona.core.Entities.Models.Seguridad;
+﻿using Microsoft.AspNetCore.Mvc;
+using rakoona.core.Services.Interfaces;
 using rakoona.dtos.Request;
+using rakoona.dtos.Response;
 using Swashbuckle.AspNetCore.Annotations;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace rakoona.webapi.Controllers.v1.Account
 {
-    [Route("api/[controller]")]
+    [Route("api/login")]
     [ApiController]
     public class LoginController : ControllerBase
     {
-        private readonly UserManager<User> _userManager;
-        private readonly IConfiguration _configuration;
+        private readonly IAccountService _accountService;
         private readonly ILogger<LoginController> _logger;
 
         public LoginController(
-            UserManager<User> userManager,
-            IConfiguration configuration,
+            IAccountService accountService,
             ILogger<LoginController> logger)
         {
-            _userManager = userManager;
-            _configuration = configuration;
+            _accountService = accountService;
             _logger = logger;
         }
 
+        
         [SwaggerOperation(Tags = new[] { "Account" })]
         [HttpPost]
-        public async Task<IActionResult> Login([FromBody] AuthenticateRequest model)
+        [ProducesResponseType(typeof(TokenResponse),StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Login([FromBody] AuthenticateRequest request)
         {
             _logger.LogInformation("Login...");
-            var user = await _userManager.FindByEmailAsync(model.Email);
-
-            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
-            {
-
-                if (user.EmailConfirmed)
-                {
-
-                }
-
-                var userRoles = await _userManager.GetRolesAsync(user);
-
-                var authClaims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, user.UserName),
-                    new Claim(ClaimTypes.Email, user.Email),
-                    new Claim(JwtRegisteredClaimNames.NameId, user.Id),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                };
-
-                foreach (var userRole in userRoles)
-                {
-                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
-                }
-
-                var token = GetToken(authClaims);
-
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(token),
-                    expiration = token.ValidTo
-                });
-            }
+            var token = await _accountService.Login(request);
+            if (token != null)
+                return Ok(token);
+            
             return Unauthorized();
-        }
-
-        private JwtSecurityToken GetToken(List<Claim> authClaims)
-        {
-            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
-
-            var token = new JwtSecurityToken(
-                issuer: _configuration["JWT:ValidIssuer"],
-                audience: _configuration["JWT:ValidAudience"],
-                expires: DateTime.Now.AddDays(3),
-                claims: authClaims,
-                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
-                );
-
-            return token;
         }
     }
 }
