5b174ec7f721293e71e1dbfe8e71e16bee467bb3
Enrique Chan <enrique.nec@gmail.com>
2023-09-19 14:32:54 -0600

    Create IAccountService.cs

---
+++ rakoona.core/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using rakoona.dtos.Request;
+using rakoona.dtos.Response;
+
+namespace rakoona.core.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<bool> ValidateIfUserExist(string email);
+        Task<Response> Register(RegisterRequest model);
+        Task<TokenResponse> Login(AuthenticateRequest model);
+    }
+}
