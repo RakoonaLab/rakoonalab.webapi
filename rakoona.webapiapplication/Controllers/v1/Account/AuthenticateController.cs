55a33572e6808fd6f3142b3b6f976ac736e8df50
Enrique Chan <enrique.nec@gmail.com>
2023-09-19 12:03:35 -0600

    Update AuthenticateController.cs

--- rakoona.webapiapplication/Controllers/v1/Account/AuthenticateController.cs
+++ rakoona.webapiapplication/Controllers/v1/Account/AuthenticateController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using rakoona.core.Entities.Models.Seguridad;
